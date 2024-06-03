6177cff721926e0b1993629444fc6711ab0ecd84
Danil <Chokomars-2000@mail.ru>
2024-06-03 18:33:44 +0500

    Несохр изменения

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
             currentRef={ref}
           />
           <UserCard curUser={curUser} />
+          <div> Изменения для переноса </div>
         </>
       )}
     </div>
